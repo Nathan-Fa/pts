b6c69358639d541c0f12fe64e7d25904346da854
Nathan-Fa <77683451+Nathan-Fa@users.noreply.github.com>
2023-03-25 13:33:23 +0100

    [ADD] Move to object detection module

--- application/src/app/modules/object-detection/object-detection.component.ts
+++ application/src/app/modules/object-detection/object-detection.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
+import {Component, ViewChild} from '@angular/core';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
 
 @Component({
   selector: 'app-object-detection',
@@ -8,4 +8,60 @@ import * as tf from '@tensorflow/tfjs';
 })
 export class ObjectDetectionComponent {
 
+  @ViewChild("video")
+  public video: any;
+  //public video: ElementRef;
+  error: any;
+  predictionValue = [{
+    class: '- - -',
+    score: 0
+  }];
+  totalObjectsDetected = 0;
+  dataLoading = false;
+
+  async ngAfterViewInit() {
+    await this.setupDevices();
+  }
+
+  // setup the media device or device camera
+  async setupDevices() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true
+        });
+        if (stream) {
+          this.video.nativeElement.srcObject = stream;
+          this.video.nativeElement.play();
+          this.error = null;
+        } else {
+          this.error = "You have no output video device";
+        }
+      } catch (e) {
+        this.error = e;
+      }
+    }
+  }
+
+  // turn off device camera
+  stopCamera() {
+    this.video.nativeElement.srcObject.getTracks().forEach((track: { stop: () => any; }) => track.stop());
+  }
+
+  // turn on device camera
+  async starCamera() {
+    await this.setupDevices();
+  }
+
+  // load the tenseflow object detection model and send video stram to model an get response
+  async loadImageDetection() {
+    this.dataLoading = true;
+    cocoSsd.load().then(model => {
+      model.detect(this.video.nativeElement).then(predictions => {
+        this.predictionValue = predictions;
+        this.totalObjectsDetected = predictions.length
+        this.dataLoading = false;
+      });
+    });
+  }
 }
